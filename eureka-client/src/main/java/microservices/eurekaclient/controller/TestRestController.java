99f0d4933ae458c958e6d40cbc85ef40748af427
dmitribaskakov <dmitri.baskakov.git@gmail.com>
2022-07-10 21:00:51 +0300

    chg api-gateway

--- eureka-client/src/main/java/microservices/eurekaclient/controller/TestRestController.java
+++ eureka-client/src/main/java/microservices/eurekaclient/controller/TestRestController.java
@@ -1,9 +1,11 @@
 package microservices.eurekaclient.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/main")
 public class TestRestController {
     @GetMapping("/test")
     public String test() {
