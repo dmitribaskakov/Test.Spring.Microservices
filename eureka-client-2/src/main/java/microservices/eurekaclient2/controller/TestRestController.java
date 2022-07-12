7bca2d5c5cc594c856cf1cf8dc77358d2d41d249
dmitribaskakov <dmitri.baskakov.git@gmail.com>
2022-07-12 11:21:11 +0300

    move application.properties to github-config-server

--- eureka-client-2/src/main/java/microservices/eurekaclient2/controller/TestRestController.java
+++ eureka-client-2/src/main/java/microservices/eurekaclient2/controller/TestRestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/new")
+@RequestMapping("/main")
 public class TestRestController {
     @GetMapping("/name")
     public String test() {
